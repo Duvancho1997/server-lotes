58cb7f7fcdd267afebd0fc00a5ee722e80c00110
duvan chantre <duvanchantree@gmail.com>
2023-01-18 09:56:40 -0500

    first commit6

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
   <React.StrictMode>
-   
+   <h1>Hola perdi la apuesta</h1>
     <App/>
    
   </React.StrictMode>
